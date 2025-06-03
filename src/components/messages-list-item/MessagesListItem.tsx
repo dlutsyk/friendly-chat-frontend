69c2caabbd809334ed51a825fbb9084db4dc37a3
Dmytro Lutsyk <d.lutsyk@astoundcommerce.com>
2025-06-03 22:39:04 +0300

    Chat structure

---
+++ src/components/messages-list-item/MessagesListItem.tsx
@@ -0,0 +1,14 @@
+const MessagesList = () => {
+  return (
+    <div>
+      <div>
+        Messages are displayed here.
+      </div>
+      <div>
+        Somebody is typing...
+      </div>
+    </div>
+  );
+}
+
+export { MessagesList };
